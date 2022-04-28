a3a1f3d042e30e0b9100a9dc498ba99886215ae5
Simei <aq.simei@gmail.com>
2022-04-28 11:07:03 -0300

    setup: create base components with styled_components

---
+++ src/screens/Dashboard/index.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+import { Container, Title } from './style' 
+export const Dashboard = () => {
+  return (
+  <Container>
+    <Title>This is my dashboard</Title>  
+  </Container>
+  )
+} 
